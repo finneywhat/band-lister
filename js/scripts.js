53621603459ebc6559c38bdcfb8cc39db8f79bad
Chris Finney and Cameron Jensen <cameron@>
2017-02-27 11:13:43 -0800

    add javascript functionality to accept user input and append band name to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
+//Business Logic
+function Band(band, album) {
+  this.band = band;
+  this.album = album;
+}
+
+
+//User Interface
 $(document).ready(function(){
+  $('form#bandForm').submit(function(event){
+    event.preventDefault();
 
+    var newBand = $("input#bandName").val();
+    var newAlbum = $("input#album").val();
 
+    var bandInfo = new Band(newBand, newAlbum);
+
+    $("#bandList").append("<li><span class='clickable'>" + bandInfo.band + "</span></li>");
+
+  });
 
 });
